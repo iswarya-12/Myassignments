e54dc5f0a591a1e2ed2089dd62d5173470e94e79
iswarya-12 <124444528+iswarya-12@users.noreply.github.com>
2023-02-04 19:11:58 +0530

    Add files via upload
    Day1assignment

---
+++ assignment/Mobile.java
@@ -0,0 +1,23 @@
+package week1.assignment;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String mobilename="samsung";
+System.out.println(mobilename);
+char mobilelogo='S';
+System.out.println(mobilelogo);
+short nomobilepiece=128;
+System.out.println(nomobilepiece);
+int modelno=12344;
+System.out.println(modelno);
+long mobileime=1234556660l;
+System.out.println(mobileime);
+float mobileprice=12000.5f;
+System.out.println(mobileprice);
+boolean isdamaged=false;
+System.out.println(isdamaged);
+	}
+
+}
---
+++ assignment/TwoWheeler.java
@@ -0,0 +1,21 @@
+package week1.assignment;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int wheel=2;
+System.out.println(wheel);
+short mirror=2
+System.out.println(mirror);
+long chassis=112224455899l;
+System.out.println(chassis);
+boolean ispuntured=false;
+System.out.println(ispuntured);
+String bikename="suzuki";
+System.out.println(bikename);
+double runningkm=123.444;
+System.out.println(runningkm);
+	}
+
+}
